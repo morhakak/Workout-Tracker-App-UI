12a27e064ea93c5eb07a2ec237c1ba1d1b7c8e00
ADS\morh <morhak6819@gmail.com>
2024-09-28 18:29:04 +0300

    Update login and register views to use one layout

---
+++ workout-tracker-app/src/components/layout/AuthLayout.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="flex gap-10 items-center justify-center overflow-x-hidden">
+      <div
+        class="w-[450px] sm:min-w-[700px] h-[550px] sm:h-[450px] flex flex-col justify-between px-4 py-6 ml-4 "
+        rounded="lg"
+      >
+        <div class="w-[80%] mx-auto ">
+         <slot></slot>
+        </div>
+        <p class="text-center text-neutral-400 font-semibold">
+         {{isLogin ?  `Don't have an account yet?` : `Already registered?`}}
+          <v-btn variant="plain" color="black" :to="isLogin ? `/auth/register` : `/auth/login`"> {{isLogin ?  `Register` : `Login`}}</v-btn>
+        </p>
+      </div>
+      <img
+        class="hidden lg:block w-[50%] h-20 min-h-screen flex-1 rounded-tl-[40px] rounded-bl-[40px]"
+        src="../../../public/assets/bench-press.png"
+      ></img>
+    </div>
+    <v-snackbar color="red" v-model="snackbarError">
+      <template #text>
+        <div class="flex items-center gap-4">
+          <v-icon>mdi-alert-circle</v-icon>
+          <p class="text-lg">{{ messages[0]?.message }}</p>
+        </div>
+      </template>
+      <template v-slot:actions>
+        <v-btn color="black" variant="text" @click="snackbarError = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </template>
+
+  <script setup>
+defineProps(["isLogin"]);
+</script>
