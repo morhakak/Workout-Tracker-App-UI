76845410f1401c295542833f426ec545c6d6423a
ADS\morh <morhak6819@gmail.com>
2024-10-14 23:06:39 +0300

    Add measurements view and MeasurementInput component

---
+++ workout-tracker-app/src/components/measurements/MeasurementInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-text-field
+    :label="label"
+    :v-model="modelValue"
+    :error-messages="errorMessages"
+    @blur="onBlur"
+    @input="onInput"
+    clearable
+    class="text-black font-semibold w-[150px]"
+    variant="outlined"
+    color="black"
+    base-color="black"
+    :suffix="suffix"
+    style="font-size: 1.125rem; color: white"
+    :hint="hint"
+  />
+</template>
+
+<script setup>
+const props = defineProps({
+  label: String,
+  modelValue: Number,
+  errorMessages: Array,
+  onBlur: Function,
+  onInput: Function,
+  suffix: { type: String, default: "" },
+  hint: String,
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const inputHandler = (value) => emit("update:modelValue", value);
+</script>
--- workout-tracker-app/src/views/MeasurementsView.vue
+++ workout-tracker-app/src/views/MeasurementsView.vue
@@ -1,5 +1,188 @@
 <template>
-  <div></div>
+  <v-container class="flex flex-col items-center">
+    <v-card class="px-6 w-[600px] rounded-xl">
+      <h1 class="text-center text-3xl text-black font-bold mt-8 uppercase">
+        Body Measurements
+      </h1>
+      <h2 class="text-center text-xl text-neutral-500 font-semibold mt-6 mb-4">
+        Height and Weight
+      </h2>
+      <form
+        @submit.prevent="submitForm"
+        class="mb-8 grid grid-cols-2 justify-items-center gap-3"
+      >
+        <MeasurementInput
+          label="Height"
+          v-model="state.height"
+          :error-messages="v$.height.$errors.map((e) => e.$message)"
+          @blur="v$.height.$touch"
+          @input="v$.height.$touch"
+          suffix="cm"
+          class="justify-self-end"
+        />
+        <MeasurementInput
+          label="Weight"
+          v-model="state.weight"
+          :error-messages="v$.weight.$errors.map((e) => e.$message)"
+          @blur="v$.weight.$touch"
+          @input="v$.weight.$touch"
+          suffix="kg"
+          class="justify-self-start"
+        />
+        <h2
+          class="text-center text-xl text-neutral-500 font-semibold col-span-2 mb-4"
+        >
+          Circumference Measurements
+        </h2>
+        <MeasurementInput
+          label="Neck"
+          v-model="state.neck"
+          :error-messages="v$.neck.$errors.map((e) => e.$message)"
+          @blur="v$.neck.$touch"
+          @input="v$.neck.$touch"
+          suffix="cm"
+          class="justify-self-end"
+        />
+        <MeasurementInput
+          label="Shoulders"
+          v-model="state.shoulders"
+          :error-messages="v$.shoulders.$errors.map((e) => e.$message)"
+          @blur="v$.shoulders.$touch"
+          @input="v$.shoulders.$touch"
+          suffix="cm"
+          class="justify-self-start"
+        />
+        <MeasurementInput
+          label="Chest"
+          v-model="state.chest"
+          :error-messages="v$.chest.$errors.map((e) => e.$message)"
+          @blur="v$.chest.$touch"
+          @input="v$.chest.$touch"
+          suffix="cm"
+          class="justify-self-end"
+        />
+        <MeasurementInput
+          label="Right Arm"
+          v-model="state.rightArm"
+          :error-messages="v$.rightArm.$errors.map((e) => e.$message)"
+          @blur="v$.rightArm.$touch"
+          @input="v$.rightArm.$touch"
+          suffix="cm"
+          class="justify-self-start"
+        />
+        <MeasurementInput
+          label="Left Arm"
+          v-model="state.leftArm"
+          :error-messages="v$.leftArm.$errors.map((e) => e.$message)"
+          @blur="v$.leftArm.$touch"
+          @input="v$.leftArm.$touch"
+          suffix="cm"
+          class="justify-self-end"
+        />
+        <MeasurementInput
+          label="Waist"
+          v-model="state.waist"
+          :error-messages="v$.waist.$errors.map((e) => e.$message)"
+          @blur="v$.waist.$touch"
+          @input="v$.waist.$touch"
+          suffix="cm"
+          class="justify-self-start"
+        />
+        <MeasurementInput
+          label="Right Thigh"
+          v-model="state.rightThigh"
+          :error-messages="v$.rightThigh.$errors.map((e) => e.$message)"
+          @blur="v$.rightThigh.$touch"
+          @input="v$.rightThigh.$touch"
+          suffix="cm"
+          class="justify-self-end"
+        />
+        <MeasurementInput
+          label="Left Thigh"
+          v-model="state.leftThigh"
+          :error-messages="v$.leftThigh.$errors.map((e) => e.$message)"
+          @blur="v$.leftThigh.$touch"
+          @input="v$.leftThigh.$touch"
+          suffix="cm"
+          class="justify-self-start"
+        />
+        <MeasurementInput
+          label="Right Calf"
+          v-model="state.rightCalf"
+          :error-messages="v$.rightCalf.$errors.map((e) => e.$message)"
+          @blur="v$.rightCalf.$touch"
+          @input="v$.rightCalf.$touch"
+          suffix="cm"
+          class="justify-self-end"
+        />
+        <MeasurementInput
+          label="Left Calf"
+          v-model="state.leftCalf"
+          :error-messages="v$.leftCalf.$errors.map((e) => e.$message)"
+          @blur="v$.leftCalf.$touch"
+          @input="v$.leftCalf.$touch"
+          suffix="cm"
+          class="justify-self-start"
+        />
+        <v-btn
+          color="black"
+          class="text-lg mt-4 col-span-2 w-[57%]"
+          height="45"
+          type="submit"
+        >
+          Add Measuremnt
+        </v-btn>
+      </form>
+    </v-card>
+  </v-container>
 </template>
 
-<script setup></script>
+<script setup>
+import { useVuelidate } from "@vuelidate/core";
+import { numeric, required } from "@vuelidate/validators";
+import { reactive } from "vue";
+import MeasurementInput from "../components/measurements/MeasurementInput.vue";
+
+const initialState = {
+  height: 0.0,
+  weight: 0.0,
+  neck: 0.0,
+  shoulders: 0.0,
+  chest: 0.0,
+  rightArm: 0.0,
+  leftArm: 0.0,
+  waist: 0.0,
+  rightThigh: 0.0,
+  leftThigh: 0.0,
+  rightCalf: 0.0,
+  leftCalf: 0.0,
+};
+
+const state = reactive({
+  ...initialState,
+});
+
+const rules = {
+  height: { required, numeric },
+  weight: { required, numeric },
+  neck: { required, numeric },
+  shoulders: { required, numeric },
+  chest: { required, numeric },
+  rightArm: { required, numeric },
+  leftArm: { required, numeric },
+  waist: { required, numeric },
+  rightThigh: { required, numeric },
+  leftThigh: { required, numeric },
+  rightCalf: { required, numeric },
+  leftCalf: { required, numeric },
+};
+
+const v$ = useVuelidate(rules, state);
+
+async function submitForm() {
+  v$.value.$validate();
+
+  if (!v$.value.$invalid) {
+  }
+}
+</script>
