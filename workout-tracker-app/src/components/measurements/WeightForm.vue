b1170471b4e1da8a38b505715b31810939a6c24a
ADS\morh <morhak6819@gmail.com>
2024-10-26 18:11:50 +0300

    Add tooltip with guidelines to weight form

--- workout-tracker-app/src/components/measurements/WeightForm.vue
+++ workout-tracker-app/src/components/measurements/WeightForm.vue
@@ -1,5 +1,22 @@
 <template>
   <form @submit.prevent="submitForm" class="flex gap-2 px-12">
+    <v-tooltip v-model="show" location="left">
+      <template v-slot:activator="{ props }">
+        <v-icon v-bind="props" color="grey-lighten-1">
+          mdi-help-circle-outline</v-icon
+        >
+      </template>
+      <div>
+        <p>Guidelines:</p>
+        <p>1. Weigh yourself once a week</p>
+        <p>2. Weigh yourself in the morning</p>
+        <p>3. Track your progress</p>
+        <p>4. Invest in a smart scale</p>
+        <p class="text-wrap">
+          5. Stop, only if it doesnt trigger anxiety or disordered eating
+        </p>
+      </div>
+    </v-tooltip>
     <MeasurementInput
       label="Weight"
       v-model="state.weight"
@@ -26,13 +43,14 @@ import MeasurementInput from "./MeasurementInput.vue";
 import { useVuelidate } from "@vuelidate/core";
 import { numeric, helpers } from "@vuelidate/validators";
 import { storeToRefs } from "pinia";
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
 import { useUnitUtils } from "../../stores/unitUtilsStore";
 import { useWeighingsStore } from "../../stores/weighingsStore";
 
 const weighingsStore = useWeighingsStore();
 const { isAdding } = storeToRefs(weighingsStore);
 const { weightSuffix } = storeToRefs(useUnitUtils());
+const show = ref(false);
 
 const initialState = {
   weight: 0.0,
